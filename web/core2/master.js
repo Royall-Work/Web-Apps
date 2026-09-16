2add72c32005c66a84b2c4a5f2f635c79526cdf4
Royall-Work <araafroyall.dual@gmail.com>
2026-09-16 21:42:08 +0530

    Remove Core2 and sample test files (#7)

--- web/core2/master.js
+++
@@ -1,9 +0,0 @@
-import {attachHeader} from './header.js';
-import './toast.js';
-
-const RoyallUI={};
-attachHeader(RoyallUI);
-RoyallUI.toast=window.RoyallToast;
-
-window.RoyallUI=RoyallUI;
-export {RoyallUI};
