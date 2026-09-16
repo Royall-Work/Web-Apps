9b1a61fb605d0bf98ec07ccfd6276869a2813015
Royall-Work <araafroyall.dual@gmail.com>
2026-09-16 17:33:50 +0530

    Link Core2 toast through master

--- web/core2/master.js
+++ web/core2/master.js
@@ -1,8 +1,9 @@
 import {attachHeader} from './header.js';
+import './toast.js';
 
 const RoyallUI={};
-
 attachHeader(RoyallUI);
+RoyallUI.toast=window.RoyallToast;
 
 window.RoyallUI=RoyallUI;
 export {RoyallUI};
